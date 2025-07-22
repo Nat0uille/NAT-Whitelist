fffd21504a963db58f09b1895d7eb8f9c61f42b3
Natanael <nat0uillepro@gmail.com>
2025-07-22 19:14:00 +0200

    Add WhitelistCommand class for command handling

---
+++ src/main/java/fr/Nat0uille/NATWhitelist/Commands/WhitelistCommand.java
@@ -0,0 +1,13 @@
+package fr.Nat0uille.NATWhitelist.Commands;
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.Command;
+
+
+public class WhitelistCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+}
