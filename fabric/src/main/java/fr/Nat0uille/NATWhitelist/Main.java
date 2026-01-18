94e55a76606e02316878c6588c257ee84e15166a
Natanael <nat0uillepro@gmail.com>
2026-01-18 17:44:17 +0100

    Add Fabric support and update project configuration for multi-version compatibility

---
+++ fabric/src/main/java/fr/Nat0uille/NATWhitelist/Main.java
@@ -0,0 +1,10 @@
+package fr.Nat0uille.NATWhitelist;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
